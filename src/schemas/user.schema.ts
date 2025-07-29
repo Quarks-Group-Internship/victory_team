cb0b5321aa5af0790a48c508565cfde72cbac8da
JoeBright1619 <brightcollin1619@gmail.com>
2025-07-29 12:31:07 +0200

    feat: update user validation schema

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -2,8 +2,12 @@ import { z } from "zod";
 
 // Create schema for creating a user
 export const createUserSchema = z.object({
-  firstname: z.string().min(1, "Firstname is required"),
-  lastname: z.string().min(1, "Lastname is required"),
+  firstname: z
+    .string()
+    .min(2, "Firstname can't be an empty string or less than 2 character"),
+  lastname: z
+    .string()
+    .min(2, "Lastname can't be an empty string or less than 2 character"),
   phone: z.string().min(10, "Phone must be at least 10 digits"),
   email: z.string().email("Invalid email"),
   password: z.string().min(6, "Password must be at least 6 characters"),
@@ -14,12 +18,21 @@ export const createUserSchema = z.object({
 
 // Create schema for updating a user
 export const updateUserSchema = z.object({
-  firstname: z.string().optional(),
-  lastname: z.string().optional(),
-  phone: z.string().optional(),
+  firstname: z
+    .string()
+    .min(2, "Firstname can't be an empty string or less than 2 character")
+    .optional(),
+  lastname: z
+    .string()
+    .min(2, "Lastname can't be an empty string or less than 2 character")
+    .optional(),
+  phone: z.string().min(10, "Phone must be at least 10 digits").optional(),
   email: z.string().email("Invalid email").optional(),
-  password: z.string().min(6).optional(),
-  role: z.enum(["admin", "owner", "buyer"]).optional(),
+  password: z
+    .string()
+    .min(6, "Password must be at least 6 characters")
+    .optional(),
+  roleName: z.enum(["admin", "owner", "buyer"]).optional(),
   updatedAt: z.date().optional(),
 });
 
